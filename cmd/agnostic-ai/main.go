c198c2f43caecbdbb3c4910914ca0fa57f1d8a0e
Chemaclass <chemaclass@outlook.es>
2026-08-09 19:27:56 +0200

    chore(release): v0.48.1

--- cmd/agnostic-ai/main.go
+++ cmd/agnostic-ai/main.go
@@ -7,7 +7,7 @@ import (
 	"github.com/chemaclass/agnostic-ai/internal/cli"
 )
 
-var version = "0.48.0"
+var version = "0.48.1"
 
 func main() {
 	if err := cli.NewRootCmd(version).Execute(); err != nil {
